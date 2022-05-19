ae3463680de9e15ab26d3c5d72d72ff83a7e381c
Sidplayer <sidney.sgq@gmail.com>
2022-05-19 19:10:57 -0300

    atividade_php

---
+++ Atividade01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+<?php 
+
+
+
+$r2 = 3.1415 * (4**2);
+
+$altura = 0.60;
+
+$volumeL = $r2 *$altura;
+
+echo "O volume da lata de óleo é: {$volumeL}"
+
+
+
+ ?>
+
+
+
+
+</body>
+</html>
